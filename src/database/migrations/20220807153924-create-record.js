01c288d298f6a3e033922a455d597a7f8c3d7b9f
Adson Gomes Oliveira <agdev@DESKTOP-0N655A3.localdomain>
2022-08-07 14:08:43 -0300

    <feat>: New migration added
    The model, migration and seeder for records table was created. This table have a relation with users and schemas.

---
+++ src/database/migrations/20220807153924-create-record.js
@@ -0,0 +1,50 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('records', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'user_id',
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      schemaId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'schema_id',
+        references: {
+          model: 'schemas',
+          key: 'id',
+        },
+      },
+      duration: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      rate: {
+        type: Sequelize.INTEGER,
+      },
+      note: {
+        type: Sequelize.TEXT,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('records');
+  }
+};
