156158395553b030cb56f00e28dbfdf758ceaa8a
Adson Gomes Oliveira <agdev@DESKTOP-0N655A3.localdomain>
2022-08-08 17:15:54 -0300

    <feat>: New model and migration
    Was created schema middlewares with a new model and migration.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const Users = require('./userRoutes');
 const Exercises = require('./exerciseRoutes');
+const Schema = require('./schemaRoutes');
 
 module.exports = {
   Users,
   Exercises,
+  Schema,
 };
